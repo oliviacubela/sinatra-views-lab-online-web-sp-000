1acb284b832054de54b6a20124f2308ac11d9c67
Olivia Cubela <oliviacubela@gmail.com>
2020-09-20 22:27:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get "/date" do
+		erb :date
 	end
 
+	get "/hello" do
+		erb :hello
+	end
+
+	get "/goodbye" do
+		erb :goodbye
+	end
 
 end
